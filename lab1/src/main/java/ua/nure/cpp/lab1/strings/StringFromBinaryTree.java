a2a111a1ccb3be0f5f2228ebabfe166408f61687
Bohdan Rudenko <brudenko2804@gmail.com>
2024-02-26 16:53:47 +0200

    Implement StringFromBinaryTree and add tests
    This commit implements the StringFromBinaryTree class according to the task description. It includes a method to build a string representation of a binary tree. Corresponding tests have also been added to verify the correct functionality of the implementation.

--- lab1/src/main/java/ua/nure/cpp/lab1/strings/StringFromBinaryTree.java
+++ lab1/src/main/java/ua/nure/cpp/lab1/strings/StringFromBinaryTree.java
@@ -3,7 +3,34 @@ package ua.nure.cpp.lab1.strings;
 import static ua.nure.cpp.lab1.strings.BinaryTree.*;
 
 public class StringFromBinaryTree {
+    StringBuilder sb;
+
+    public StringFromBinaryTree() {
+        this.sb = new StringBuilder();
+    }
+
     public String build(TreeNode root) {
-        throw new UnsupportedOperationException();
+        helperTraversal(root);
+        return sb.toString();
+    }
+
+    private void helperTraversal(TreeNode node) {
+        if (node == null) {
+            return;
+        }
+
+        sb.append(node.val);
+
+        if (node.left != null || node.right != null) {
+            sb.append("(");
+            helperTraversal(node.left);
+            sb.append(")");
+        }
+
+        if (node.right != null) {
+            sb.append("(");
+            helperTraversal(node.right);
+            sb.append(")");
+        }
     }
 }
--- lab1/src/test/java/ua/nure/cpp/lab1/strings/StringFromBinaryTreeTest.java
+++ lab1/src/test/java/ua/nure/cpp/lab1/strings/StringFromBinaryTreeTest.java
@@ -1,14 +1,17 @@
 package ua.nure.cpp.lab1.strings;
 
+import org.junit.jupiter.api.Disabled;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
+import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertThrows;
 import static ua.nure.cpp.lab1.strings.BinaryTree.*;
 
 class StringFromBinaryTreeTest {
 
     //Disable this test after you have implemented the method
+    @Disabled("Disabled due to method implementation")
     @Test
     @DisplayName("Should throw UnsupportedOperationException when not implemented")
     void shouldThrowUnsupportedOperationException() {
@@ -16,5 +19,62 @@ class StringFromBinaryTreeTest {
         BinaryTree binaryTree = new BinaryTree(root);
         StringFromBinaryTree stringFromBinaryTree = new StringFromBinaryTree();
         assertThrows(UnsupportedOperationException.class, () -> stringFromBinaryTree.build(binaryTree.root));
+    }
+
+    @Test
+    @DisplayName("Test case 1: root = [1,2,3,4]")
+    void testCase1() {
+        TreeNode node4 = new TreeNode(4);
+        TreeNode node2 = new TreeNode(2, node4, null);
+        TreeNode node3 = new TreeNode(3);
+        TreeNode root = new TreeNode(1, node2, node3);
+        BinaryTree binaryTree = new BinaryTree(root);
+        StringFromBinaryTree stringFromBinaryTree = new StringFromBinaryTree();
+        String result = stringFromBinaryTree.build(binaryTree.root);
+        assertEquals("1(2(4))(3)", result);
+    }
+
+    @Test
+    @DisplayName("Test case 2: root = [1,2,3,null,4]")
+    void testCase2() {
+        TreeNode node4 = new TreeNode(4);
+        TreeNode node2 = new TreeNode(2, null, node4);
+        TreeNode node3 = new TreeNode(3);
+        TreeNode root = new TreeNode(1, node2, node3);
+        BinaryTree binaryTree = new BinaryTree(root);
+        StringFromBinaryTree stringFromBinaryTree = new StringFromBinaryTree();
+        String result = stringFromBinaryTree.build(binaryTree.root);
+        assertEquals("1(2()(4))(3)", result);
+    }
+
+    @Test
+    @DisplayName("Test case 3: root = [1]")
+    void testCase3() {
+        TreeNode root = new TreeNode(1);
+        BinaryTree binaryTree = new BinaryTree(root);
+        StringFromBinaryTree stringFromBinaryTree = new StringFromBinaryTree();
+        String result = stringFromBinaryTree.build(binaryTree.root);
+        assertEquals("1", result);
+    }
+
+    @Test
+    @DisplayName("Test case 4: root = [1,2,null,3]")
+    void testCase4() {
+        TreeNode node3 = new TreeNode(3);
+        TreeNode node2 = new TreeNode(2, node3, null);
+        TreeNode root = new TreeNode(1, node2, null);
+        BinaryTree binaryTree = new BinaryTree(root);
+        StringFromBinaryTree stringFromBinaryTree = new StringFromBinaryTree();
+        String result = stringFromBinaryTree.build(binaryTree.root);
+        assertEquals("1(2(3))", result);
+    }
+
+    @Test
+    @DisplayName("Test case 5: root = null")
+    void testCase5() {
+        BinaryTree binaryTree = new BinaryTree(null);
+        StringFromBinaryTree stringFromBinaryTree = new StringFromBinaryTree();
+        String result = stringFromBinaryTree.build(binaryTree.root);
+        assertEquals("", result);
     }
 }
