2ac6e6d9a9c41b6a1c1488b46bc52c8042fddf6a
Bohdan Rudenko <brudenko2804@gmail.com>
2024-03-07 22:36:28 +0200

    Refactor: Remove disabled test
    - Removed disabled test due to method implementation

--- lab1/src/test/java/ua/nure/cpp/lab1/strings/StringFromBinaryTreeTest.java
+++ lab1/src/test/java/ua/nure/cpp/lab1/strings/StringFromBinaryTreeTest.java
@@ -1,25 +1,12 @@
 package ua.nure.cpp.lab1.strings;
 
-import org.junit.jupiter.api.Disabled;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
-import static org.junit.jupiter.api.Assertions.assertThrows;
 import static ua.nure.cpp.lab1.strings.BinaryTree.*;
 
 class StringFromBinaryTreeTest {
-
-    //Disable this test after you have implemented the method
-    @Disabled("Disabled due to method implementation")
-    @Test
-    @DisplayName("Should throw UnsupportedOperationException when not implemented")
-    void shouldThrowUnsupportedOperationException() {
-        TreeNode root = new TreeNode(1, null, null);
-        BinaryTree binaryTree = new BinaryTree(root);
-        StringFromBinaryTree stringFromBinaryTree = new StringFromBinaryTree();
-        assertThrows(UnsupportedOperationException.class, () -> stringFromBinaryTree.build(binaryTree.root));
-    }
 
     @Test
     @DisplayName("Test case 1: root = [1,2,3,4]")
